a8101bf835721746f7ec16e0739c7fe783a0ba5c
stan05 <stanislav.petr7@gmail.com>
2020-02-03 00:03:55 +0100

    creating structure

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Layout from "./components/Layout/Layout";
+import BurgetBuilder from "./containers/BurgetBuilder/BurgetBuilder";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
-          <h1>works</h1>
+          <BurgetBuilder />
         </Layout>
       </div>
     );
